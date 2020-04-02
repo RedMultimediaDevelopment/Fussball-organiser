32f2b85bfbb5214883e1b595086d0c8b9d72c4e2
Maurice Waelen <m.waelen@student.fontys.nl>
2020-04-02 10:29:17 +0200

    Added header component + optimized file structure

---
+++ dashboard/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+
+
+
+export class Header extends React.Component {
+
+
+
+  render() {
+    return (
+      <div class="header">
+        <h1>Fussball organiser</h1>
+      </div>
+        );
+      }
+    }
